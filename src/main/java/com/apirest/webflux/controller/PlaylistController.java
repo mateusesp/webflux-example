9a27d48d6a45eb7fdb127f8cd53b8a5c8762d2fe
Mateus Esposito <m.espositof@gmail.com>
2021-12-20 19:10:05 -0300

    add reactive endpoint

--- src/main/java/com/apirest/webflux/controller/PlaylistController.java
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -3,11 +3,16 @@ package com.apirest.webflux.controller;
 import com.apirest.webflux.document.Playlist;
 import com.apirest.webflux.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
+import java.time.Duration;
+import java.util.Date;
+
+@RestController
 public class PlaylistController {
 
     @Autowired
@@ -26,5 +31,14 @@ public class PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> save(@RequestBody Playlist playlist) {
         return service.save(playlist);
+    }
+
+    @GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents() {
+
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
+        Flux<Playlist> events = service.findAll();
+        System.out.println(new Date().getTime() + ":: Went through the events");
+        return Flux.zip(interval, events);
     }
 }
